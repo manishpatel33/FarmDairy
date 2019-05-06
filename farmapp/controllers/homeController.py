752ff9f2c213123442d2352e255cc1dc23fc405c
Manish <manishpatel199820@gmail.com>
2019-05-06 14:26:33 +0530

    profile

--- farmapp/controllers/homeController.py
+++ farmapp/controllers/homeController.py
@@ -17,7 +17,8 @@ from FarmProj.helpers import validateHelper
 from django.utils.translation import ugettext_lazy as _
 from django.core.files.storage import FileSystemStorage
 
-HOME_PAGE = 'main_theme.html'
+HOME_PAGE = 'index.html'
+PROFILE_PAGE = 'profile.html'
 
 
 class index(View):
@@ -27,3 +28,49 @@ class index(View):
         return render(request, HOME_PAGE)
 
 
+class profile(View):
+
+    @farmAuth.Farm_login_required_Class
+    def get(self, request):
+        return render(request, PROFILE_PAGE)
+
+    def validate_data(self, request):
+        data = {}
+
+        full_name = request.POST.get('full_name', '')
+        if full_name == '':
+            raise ValidationError('Full Name Is Empty')
+        data['full_name'] = full_name
+
+        email = request.POST.get('email', '')
+        if not validateHelper.validate_email(email):
+            raise ValidationError('Enter Valid Email ')
+        data['email'] = email
+
+        contact_no = request.POST.get('contact_no', '')
+        if not validateHelper.validate_mobile(contact_no):
+            raise ValidationError('Enter valid Contact number')
+        data['contact_no'] = contact_no
+
+        return data
+
+    @farmAuth.Farm_login_required_Class
+    def post(self, request):
+
+        try:
+            data = self.validate_data(request)
+        except ValidationError as e:
+            messages.error(request, e.message)
+
+            return HttpResponseRedirect(reverse('farmApp:profile'))
+        except Exception as e:
+            messages.error(request, e.message)
+            return HttpResponseRedirect(reverse('farmApp:profile'))
+
+        farmuser = request.farm_user
+        farmuser.full_name = data['full_name']
+        farmuser.email = data['email']
+        farmuser.contact_no = data['contact_no']
+        farmuser.save()
+        messages.success(request, "Changes Successfully Save")
+        return HttpResponseRedirect(reverse('farmApp:profile'))
